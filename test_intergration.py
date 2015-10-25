5c557640f5715d142792555c998e507ab31e5d00
Vincent Maillol <vincent.maillol@gmail.com>
2015-10-25 18:34:44 +0100

    update test_integration

--- test_intergration.py
+++ test_intergration.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import unittest
+from imp import reload
+
 import os
 from tempfile import TemporaryDirectory
 import json
@@ -15,23 +17,19 @@ class TestCrud(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        print('make DB')
+        MetaResource.clear()
+
         class Book(Resource):
             title = StringField()
             abstract = StringField()
+            chapters = ComposedBy('Chapter')
 
         class Chapter(Resource):
-            number = NumberField()
-
-        class Paragraphe(Resource):
-            number = NumberField()
-
+            number = NumberField(weak_id=True)
 
         cls.tmp_dir = TemporaryDirectory()
 
-        MetaResource._name_to_ref()
-        MetaResource._initialize_db(peewee.SqliteDatabase(os.path.join(cls.tmp_dir.name, 'test.db')))
-        MetaResource.db.connect()
+        MetaResource.initialize(peewee.SqliteDatabase(os.path.join(cls.tmp_dir.name, 'test.db')))
         MetaResource.create_tables()
         cls.server = RESTServer()
         cls.saved_books = []
@@ -70,17 +68,23 @@ class TestCrud(unittest.TestCase):
 
         created_book = self.server.post('/Book/', '', book)
         created_book = json.loads(created_book.decode('utf-8'))
-        #import pprint
-        #print("°"* 30)
-        #pprint.pprint(created_book)
 
         self.saved_books.append(expected_book)
         self.assertEqual(created_book, expected_book)
 
+    def test_O5_add_chapter_to_book_1(self):
+        chapter = dict(number=1, book_id=1)
+        expected_chapter = dict(
+            number=1,
+            book_id={'id': 1,
+                     'title': 'Martine to the cinema',
+                     'abstract': 'Martine go to the cinema with Chuck Noris'}
+        )
+
+        created_chapter = self.server.post('/Chapter/', '', chapter)
+        created_chapter = json.loads(created_chapter.decode('utf-8'))
+        self.assertEqual(created_chapter, expected_chapter)
 
     @classmethod
     def tearDownClass(cls):
-        print('dro DB')
         cls.tmp_dir.cleanup()
-
-
